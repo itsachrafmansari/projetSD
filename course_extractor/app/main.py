803440c3bf12de01304c73754a3240842e105045
itsachrafmansari <contact@achrafmansari.com>
2025-02-25 02:43:26 +0000

    Modified main.py to accommodate my scraper and downloader  functions

--- course_extractor/app/main.py
+++ course_extractor/app/main.py
@@ -1,8 +1,16 @@
 import os
 
+from dotenv import load_dotenv
+
+from course_extractor.app.scraper.scraper import scrap_data_and_download_pdfs
 from course_extractor.app.storage.database import AtlasClient
-from course_extractor.app.storage.models import CourseDocument
-from course_extractor.app.extractors.pdf_extractor import extract_text_from_pdf, download_pdf
+from course_extractor.app.extractors.pdf_extractor import extract_text_from_pdf
+
+# Defining global variables and constants ______________________________________________________________________________
+load_dotenv()  # take environment variables from .env
+
+# Download Path
+DOWNLOAD_PATH = os.getenv("DOWNLOAD_PATH")
 
 # Mongodb SetUp
 MONGODB_URI = os.getenv("MONGODB_URI")
@@ -10,54 +18,24 @@ MONGODB_DB_NAME = os.getenv("MONGODB_DB_NAME")
 MONGODB_COLLECTION_NAME = os.getenv("MONGODB_COLLECTION_NAME")
 mongodb_client = AtlasClient(MONGODB_URI, MONGODB_DB_NAME)
 
-# Liste des URLs des PDFs à traiter
-urls = [
-    "https://urfist.pages.math.unistra.fr/cours-python/cours-python.pdf",
-    "https://xgarrido.github.io/licence_python_teaching/pdf/01_slide_environnement.pdf",
-    "https://xgarrido.github.io/licence_python_teaching/pdf/02_slide_type.pdf",
-    "https://xgarrido.github.io/licence_python_teaching/pdf/03_slide_fonction.pdf",
-    "https://xgarrido.github.io/licence_python_teaching/pdf/04_slide_module.pdf"
-]
+# Main _________________________________________________________________________________________________________________
+# scrap_data_and_download_pdfs()
 
-# Dossier de téléchargement
-download_dir = "downloads"
-documents = []  # Liste des documents à insérer
+# Fetch all the documents data stored in the database
+docs_data = mongodb_client.get_many(MONGODB_COLLECTION_NAME)
 
-for url in urls:
-    # Télécharger le PDF
-    downloaded_pdf_path = download_pdf(url, download_dir)
+# Extract text from each pdf then insert it in the database
 
-    # Vérifier si le fichier a bien été téléchargé
-    if not os.path.exists(downloaded_pdf_path):
-        print(f"Échec du téléchargement pour {url}")
+for doc in docs_data:
+    file_path = os.path.join(DOWNLOAD_PATH, doc["file_path"])
+
+    # Skip non-existing files
+    if not os.path.exists(file_path):
+        print(f"File with path ({file_path}) does not exist)")
         continue
 
-    # Extraire le texte du PDF
-    text = extract_text_from_pdf(downloaded_pdf_path)
+    # Extract text from the PDF file
+    text = extract_text_from_pdf(file_path)
 
-    # Déterminer le nom du fichier
-    file_name = os.path.basename(url)
-
-    # Métadonnées associées
-    metadata = {
-        "title": f"Document extrait de {file_name}",
-        "source": url
-    }
-
-    # Contenu du document
-    content = {
-        "text": text
-    }
-
-    tags = ["python", "programming"]
-
-    # Créer un objet CourseDocument et l'ajouter à la liste
-    course_doc = CourseDocument(file_name, "pdf", metadata, content, tags)
-    documents.append(course_doc)
-
-# Insérer tous les documents en une seule opération
-if documents:
-    inserted_ids = mongodb_client.insert_many(MONGODB_COLLECTION_NAME, [doc.to_dict() for doc in documents], ignore_duplicates=True)
-    print(f"{len(inserted_ids)} documents insérés avec succès.")
-else:
-    print("Aucun document à insérer.")
+    # Insert the extracted PDF file to the database
+    mongodb_client.update_one(MONGODB_COLLECTION_NAME, doc["_id"], {"text": text})
